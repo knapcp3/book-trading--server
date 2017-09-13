35be3810cf573ca1fe7719576a9851b5a1c08655
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-09-13 11:33:33 +0200

    Generate token on signup

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,11 @@
+const JWT = require('jsonwebtoken')
+
 const User = require('../models/user')
+const { JWT_SECRET } = require('../config')
+
+const signToken = user => {
+  return JWT.sign({ sub: user._id }, JWT_SECRET)
+}
 
 module.exports = {
   signUp: async (req, res, next) => {
@@ -8,7 +15,9 @@ module.exports = {
     const user = await newUser.save()
     const picked = (({ username, _id }) => ({ username, _id }))(user)
 
-    res.json(user)
+    const token = signToken(user)
+
+    res.header('authorization', token).json(picked)
   },
 
   logIn: async (req, res, next) => {
